f2c44165c777329d07cc9a876854376a420b1e02
Man-from-earth25 <manfromearth25@gmail.com>
2022-07-11 04:35:03 +0600

    Automated Commit By Friday

--- git.sh
+++ git.sh
@@ -2,25 +2,6 @@
 
 
 echo "////// WelCome To FriDay AutoMates //////"
-
-function stopwatch() {
-    local BEGIN=$(date +%s)
-    echo Starting Stopwatch...
-
-    while true; do
-        local NOW=$(date +%s)
-        local DIFF=$(($NOW - $BEGIN))
-        local MINS=$(($DIFF / 60))
-        local SECS=$(($DIFF % 60))
-        local HOURS=$(($DIFF / 3600))
-        local DAYS=$(($DIFF / 86400))
-
-        printf "\r%3d Days, %02d:%02d:%02d" $DAYS $HOURS $MINS $SECS
-        sleep 0.5
-    done
-}
-
-
 npm run fy &&
 git pull g main
 
@@ -28,4 +9,31 @@ sleep 1m
 npm run fy
 git pull g main
 
+sleep 1m
+npm run fy
+git pull g main
+
+sleep 1m
+npm run fy
+git pull g main
+
+sleep 1m
+npm run fy
+git pull g main
+
+sleep 1m
+npm run fy
+git pull g main
+
+sleep 1m
+npm run fy
+git pull g main
+
+sleep 1m
+npm run fy
+git pull g main
+
+sleep 1m
+npm run fy
+git pull g main
 
