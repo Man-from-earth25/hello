87f57230b24666ff7e666a29b2ffb948201ee869
manfromearth25@gmail.com <manfromearth25@gmail.com>
2022-07-12 19:59:51 +0600

    www

--- web/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../ui/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../ui/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
