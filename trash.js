ba10152ca77997d13feec7f4465cf414cd13214d
manfromearth25@gmail.com <manfromearth25@gmail.com>
2022-07-17 18:12:22 +0600

    Network is different

--- trash.js
+++ trash.js
@@ -18,3 +18,40 @@
 // console.table(hackIn.children.length);
 // console.table(hackIn.firstElementChild.className);
 // console.table(hackIn.lastElementChild.className);
+
+
+
+
+// Regex
+// let text = 'do to good experience blockchain to related t amily';
+// let regex = /to/g;
+// let result = text.replace(regex,`(I donnot know) but probebly this words needs ${regex}`);
+// console.log(result);
+// Regex
+// let text = 'do to good experience blockchain to related t amily';
+// let regex = /to/g;
+// let result = text.replace(regex,`(I donnot know) but probebly this words needs ${regex}`);
+// console.log(result);
+
+// Behave && Eruda console can handle it && Eruda says "I am gooddd"
+// let html = document.querySelector('body');
+// html.style.display = 'none';
+// // Browser
+// let td = document.querySelectorAll('a > div');
+// td.forEach((elements) => {
+//     console.log(elements.innerHTML);
+// });
+
+// let bn = document.querySelectorAll('.MuiPaper-root:nth-child(1)');
+// bn.forEach((elements) => {
+//     console.log(elements.length);
+// });
+
+
+
+
+// let bn = document.querySelectorAll('.MuiPaper-root:nth-child(1),..MuiPaper-root:nth-child(1) > *');
+// bn.forEach((elements) => {
+//     console.log(elements.length);
+// });
+// console.log(bn);
