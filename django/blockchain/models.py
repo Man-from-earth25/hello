77df537264e23133502df029e14c53052d4c2461
Man-from-earth25 <manfromearth25@gmail.com>
2022-07-18 10:14:58 +0600

    Hello

--- django/blockchain/models.py
+++ django/blockchain/models.py
@@ -2,18 +2,8 @@ from django.db import models
 from datetime import datetime
 
 name = "Python"
-def handle_uploaded_file(self, f):
-    _datetime = datetime.now()
-    datetime_str = _datetime.strftime("%Y-%m-%d-%H-%M-%S")
-    # if there are more than one dots
-    file_name_split = f.name.split('.')
-    file_name_list = file_name_split[:-1]
-    ext = file_name_split[-1]
-    file_name_wo_ext = '.'.join(file_name_list)
-    name = '/files/{0}-{1}.{2}'.format(file_name_wo_ext, datetime_str, ext)
-    print(name)
-print(name)
-handle_uploaded_file()
+op1 = "Emon"
+op2 = "emon"
 
 
 # Blockchains only for Test.
